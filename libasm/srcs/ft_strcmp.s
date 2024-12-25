1f0f65c055ec60b0c0ff798316957c48879b10df
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2024-12-26 04:15:03 +0900

    [refactor]: edit function description

--- libasm/srcs/ft_strcmp.s
+++ libasm/srcs/ft_strcmp.s
@@ -2,9 +2,12 @@ global ft_strcmp
 
 section .text
 ; Parameters
-; 	(str1 = rdi, str1 = rsi)
+; 	(str1 = rdi, str2 = rsi)
 ; Return
-; 	rax: 0 if strings are equal, < 0 if str1 is less than str2, > 0 if str1 is more than str2
+; 	rax:
+; 		0 if strings are equal,
+; 		< 0 if str1 is less than str2,
+; 		> 0 if str1 is more than str2
 ft_strcmp:
 	xor rcx, rcx
 	xor rax, rax
@@ -18,6 +21,7 @@ ft_strcmp:
 	mov al, bl
 	cmp bl, 0
 	jne .end
+
 	inc rcx
 	jmp .loop
 .end:
