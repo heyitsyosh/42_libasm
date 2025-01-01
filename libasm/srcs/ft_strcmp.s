b67a3e0d5f05c6a6ee4b5d077062af3ad4924d83
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-01-02 02:42:05 +0900

    [refactor]: Add detailed comment block, change je/jne to jz/jnz for readability

--- libasm/srcs/ft_strcmp.s
+++ libasm/srcs/ft_strcmp.s
@@ -1,26 +1,34 @@
 global ft_strcmp
 
 section .text
-; Parameters
-; 	(str1 = rdi, str2 = rsi)
-; Return
-; 	rax:
-; 		0 if strings are equal,
-; 		< 0 if str1 is less than str2,
-; 		> 0 if str1 is more than str2
+; C Prototype:
+;    int strcmp(const char *s1, const char *s2);
+;
+; Description:
+;    Compares two strings lexicographically.
+;
+; Parameters:
+;    rdi: Pointer to the first string (s1)
+;    rsi: Pointer to the second string (s2)
+;
+; Return:
+;    rax:
+;      0  if s1 == s2
+;      <0  if s1 < s2
+;      >0  if s1 > s2
 ft_strcmp:
 	xor rcx, rcx
 	xor rax, rax
 .loop:
 	mov bl, byte [rdi + rcx]
 	test bl, bl
-	je .end
+	jz .end
 
 	mov bh, byte [rsi + rcx]
 	sub bl, bh
 	mov al, bl
 	cmp bl, 0
-	jne .end
+	jnz .end
 
 	inc rcx
 	jmp .loop
