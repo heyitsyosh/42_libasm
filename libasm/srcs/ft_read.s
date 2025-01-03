86c38ca968847c8d2846e03756f14fb0a3614c55
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-01-04 02:30:17 +0900

    [feat]: Add read and write implementation

---
+++ libasm/srcs/ft_read.s
@@ -0,0 +1,31 @@
+global ft_read
+
+extern errno_location
+
+section .text
+; C Prototype:
+;    ssize_t read(int fd, void *buf, size_t count);
+;
+; Description:
+;    Reads data from a file descriptor into a buffer.
+;
+; Parameters:
+;    rdi: File descriptor to read from
+;    rsi: Pointer to buffer
+;    rdx: Number of bytes to read
+;
+; Return value:
+;    rax: Number of bytes read (>= 0), or -1 on error
+ft_read:
+	mov rax, 0
+	syscall
+	jz .error_handle
+	ret
+
+.error_handle:
+	neg rax
+	mov rdi, rax
+	call errno_location wrt ..plt
+	mov [ rax ], rdi
+	mov rax, -1
+	ret
