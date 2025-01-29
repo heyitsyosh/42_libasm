4586a0ac7587e97f8a1a0de79b86847c2e98beb9
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-01-30 01:32:41 +0900

    [feat]: add strdup

---
+++ libasm/srcs/ft_strdup.s
@@ -0,0 +1,41 @@
+global ft_strdup
+
+extern ft_strlen
+extern ft_strcpy
+extern malloc
+extern __errno_location
+
+section .text
+; C Prototype:
+;    char *strdup(const char *s);
+;
+; Description:
+;    Duplicates a string into newly allocated memory.
+;
+; Parameters:
+;    rdi: Pointer to the source string
+;
+; Return:
+;    rax: Pointer to the newly allocated duplicate string
+;         (NULL if memory allocation fails)
+ft_strdup:
+	call ft_strlen
+	push rdi
+	inc rax
+	mov rdi, rax
+	call malloc wrt ..plt
+	jz .error_handle
+
+	pop rsi
+	mov rdi, rax
+	call ft_strcpy
+	ret
+
+.error_handle:
+	neg rax
+	mov rdi, rax
+	call errno_location wrt ..plt
+	mov [ rax ], rdi
+	mov rax, -1
+	ret
+; need to pop in error handle?
