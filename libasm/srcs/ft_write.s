2bdc42d2cf3e4a7f6e9e94d1e3dec3c3e221b0a5
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-01-20 11:41:53 +0100

    [fix]: Save errno safely

--- libasm/srcs/ft_write.s
+++ libasm/srcs/ft_write.s
@@ -25,11 +25,9 @@ ft_write:
 
 .error_handle:
 	neg rax
-	mov rdi, rax
-
-	sub rsp, 8 ; 16-byte align stack for call
+	push rax	; save errno in stack and align stack to 16-byte
 	call __errno_location wrt ..plt
-	add rsp, 8
-
+	pop rdi
+	mov [rax], edi
 	mov rax, -1
 	ret
