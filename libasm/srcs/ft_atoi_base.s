0796914432ea185e1f250ddf8380714f5bb3c774
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-02-10 19:03:17 +0100

    [feat]: Complete atoi_base

--- libasm/srcs/ft_atoi_base.s
+++ libasm/srcs/ft_atoi_base.s
@@ -51,12 +51,12 @@ ft_atoi_base:
 	inc ecx				; j++
 	mov r9b, byte [rsi + rcx]
 	test r9b, r9b
-	jz .increment_base_index	; No duplicates found, continue validation
+	jz .validate_base_increment	; No duplicates found, continue validation
 
 	cmp r9b, dl
 	je .error_end		; Duplicate found in base
 	jmp .check_for_duplicates_loop
-.increment_base_index:
+.validate_base_increment:
 	inc r8d				; i++
 	jmp .validate_base_loop
 .validate_length:
@@ -64,47 +64,55 @@ ft_atoi_base:
 	jb .error_end		; Ensure that base_len >= 2
 
 ; Parse and convert input string
-	xor eax, eax		; Initialize return value to 0
-	mov r9d, 1			; Initialize sign to 1 (positive)
+	xor eax, eax		; return_value = 0
+	mov r9d, 1			; sign = +1
 						; r8d = base_len
-.skip_spaces:
+.skip_spaces_loop:
 	mov dl, byte [rdi]
-
 	cmp dl, ' '
-	je .skip_spaces_loop
+	je .skip_spaces_increment
 	cmp dl, 9
-	jb .set_sign
+	jb .parse_sign_loop
 	cmp dl, 13
-	ja .set_sign
+	ja .parse_sign_loop
+.skip_spaces_increment:
 	inc rdi
-	jmp .skip_spaces
-.set_sign:
-	mov dl, byte [rdi]
+	jmp .skip_spaces_loop
 
+.parse_sign_loop:
+	mov dl, byte [rdi]
 	cmp dl, '+'
-	je .set_sign_loop
+	je .parse_sign_increment
 	cmp dl, '-'
 	jne .convert
-
 	neg r9d
-.set_sign_loop:
+.parse_sign_increment:
 	inc rdi
-	jmp .parse_sign
+	jmp .parse_sign_loop
+
 .convert:
 	mov dl, byte [rdi]
 	test dl, dl
-	jz .apply_sign
-
-	; get current_digit
-
-	mul eax, r8d
-	add eax, current_digit
+	jz .apply_sign			; if (str[i] == '\0') → apply sign
+	xor ecx, ecx
+.find_digit_loop:
+	mov r10b, [rsi + rcx]	; Isolate one digit
+	test r10b, r10b
+	jz .apply_sign			; Digit to convert was not in base → apply sign
+	cmp r10b, dl			; Find digit in base
+	je .add_one_digit
+	inc ecx
+	jmp .find_digit_loop
+.add_one_digit:
+	imul eax, r8d
+	add eax, ecx
 	inc rdi
 	jmp .convert
+
 .apply_sign:
 	cmp r9d, 0
 	jge .end
-	neg rax
+	neg eax
 .end:
 	ret
 .error_end:
