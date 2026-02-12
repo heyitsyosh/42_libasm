b00e208dbff7cfd3845f20e6be04bff1335a64fc
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-02-12 19:24:36 +0100

    [feat]: Add ft_list_remove_if implementation

--- libasm/srcs/ft_list_remove_if.s
+++ libasm/srcs/ft_list_remove_if.s
@@ -1,13 +1,16 @@
 global ft_list_remove_if
 
-; extern free
+extern free
 
 %include "structs.inc"
+
+%define PREV r12
+%define NEXT r13
 
 section .text
 ; C Prototype:
 ;    void ft_list_remove_if(t_list **head, void *data_ref,
-;                           int (*cmp)(), void (*free_fct)(void *));
+;                           int (*cmp)(data, data_ref), void (*free_fct)(void *));
 ; 
 ; Description:
 ;    Removes elements from a linked list if data matches 'data_ref'.
@@ -24,4 +27,74 @@ section .text
 ; Return value:
 ;    None.
 ft_list_remove_if:
-	;
+	test rdi, rdi	; Null guard list head
+	jz .end
+	test rdx, rdx	; Null guard comparison function
+	jz .end
+
+	push rbx		; Save original state of callee-saved registers
+	push NEXT
+	push PREV
+
+	xor PREV, PREV
+	mov rbx, [rdi]	; Dereference list head
+.loop:
+	test rbx, rbx
+	jz .restore		; No node
+
+	push rdi		; Save parameters
+	push rsi
+	push rdx
+	push rcx
+
+	mov rdi, [rbx + t_list.data]
+	call rdx
+
+	pop rcx			; Restore parameters
+	pop rdx
+	pop rsi
+	pop rdi
+
+	test rax, rax
+	jz .remove_node
+
+	mov PREV, rbx
+	mov rbx, [rbx + t_list.next]
+	jmp .loop
+.remove_node:
+	mov NEXT, [rbx + t_list.next]
+	push rdi		; Save parameters
+	push rsi
+	push rdx
+	push rcx
+
+	test rcx, rcx
+	jz .skip_free_function
+	mov rdi, [rbx + t_list.data]
+	call rcx
+
+.skip_free_function:
+	mov rdi, rbx
+	call free wrt ..plt
+
+	pop rcx			; Restore parameters
+	pop rdx
+	pop rsi
+	pop rdi
+
+	test PREV, PREV
+	jz .update_head
+
+	mov [PREV + t_list.next], NEXT
+	mov rbx, NEXT
+	jmp .loop
+.update_head:
+	mov [rdi], NEXT
+	mov rbx, NEXT
+	jmp .loop
+.restore:
+	pop PREV
+	pop NEXT
+	pop rbx
+.end:
+	ret
